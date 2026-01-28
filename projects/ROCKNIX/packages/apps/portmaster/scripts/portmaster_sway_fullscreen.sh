bdfa035ad6a240c46244728ce4533e227558b200
Noxwell <beebono@yahoo.com>
2026-01-28 05:11:16 -0600

    RK3566: Enable touchscreen keyboard

--- projects/ROCKNIX/packages/apps/portmaster/scripts/portmaster_sway_fullscreen.sh
+++ projects/ROCKNIX/packages/apps/portmaster/scripts/portmaster_sway_fullscreen.sh
@@ -10,4 +10,11 @@ if echo "${UI_SERVICE}" | grep -q "sway"; then
     # Explicitly map all input devices to the active game seat to prevent wayland focus revocation, and unify virtual input nodes.
     swaymsg 'seat seat1 attach "*"'
     swaymsg 'seat * keyboard_grouping smart'
+
+    if [[ "${DEVICE_HAS_DUAL_SCREEN}" == "true"]]; then
+        TSKEY=$(get_setting "rocknix.touchscreen-keyboard.enabled")
+        if [[ "${TSKEY}" == "1" ]]; then
+            swaymsg 'output DSI-1 power on, seat seat1 fallback no'
+        fi
+    fi
 fi
